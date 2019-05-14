c0bf36147c7630b4ed351883490dde2c4f1b7d57
Dorikaa <horatiu.buricea@gmail.com>
2019-05-14 16:30:49 +0300

    added all the fonts but doesnt return them good

--- text_editor.py
+++ text_editor.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import font
+from tkinter.tix import *
 import os
 import sys
 
@@ -12,8 +14,14 @@ root.title("TextEditor by horisenpai")
 text = Text(root)
 text.grid()
 
+fonts = list(font.families())
+fonts.sort()
+
 def createMain():
+   global fonts
    menubar = Menu(root)
+   scrollbar = ScrolledWindow(menubar)
+   scrollbar.pack()
    filemenu = Menu(menubar, tearoff=0)
    filemenu.add_command(label="New File", command=newfile)
    filemenu.add_command(label="Save", command=saveas)
@@ -22,10 +30,16 @@ def createMain():
 
    menubar.add_cascade(label="File", menu=filemenu)
    fontmenu = Menu(menubar, tearoff=0)
-   fontmenu.add_command(label="Helvetica", command=FontHelvetica)
+   fontmenu.add_command(label="Arial", command=FontArial)
+   fontmenu.add_command(label="Calibri", command=FontCalibri)
    fontmenu.add_command(label="Courier", command=FontCourier)
+   fontmenu.add_command(label="Courier New", command=FontCourierNew)
    fontmenu.add_command(label="TimesNewRoman", command=FontTimes)
-   fontmenu.add_command(label="Symbol", command=FontSymbol)
+   fontmenu.add_command(label="Helvetica", command=FontHelvetica)
+   fontmenu.add_separator()
+
+   for item in fonts:
+      fontmenu.add_command(label="" + item, command=GetFonts(item))
 
    menubar.add_cascade(label="Font", menu=fontmenu)
    helpmenu = Menu(menubar, tearoff=0)
@@ -77,7 +91,25 @@ def FontSymbol():
    global text
    text.config(font="Symbol")
 
-print(dir(text.config()))
+def FontArial():
+   global text
+   text.config(font="Arial")
+
+def FontCalibri():
+   global text
+   text.config(font="Calibri")
+
+def FontCourierNew():
+   global text
+   text.config(font="Courier New")
+
+def GetFonts(n):
+   global text
+   global fonts
+   for item in fonts:
+      return text.config(font="item")
+
+
 createMain()
 
 
